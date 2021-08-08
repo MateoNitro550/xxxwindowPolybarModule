2e50808430e1fbfda0a818b8829f38c3d5896b8b
MateoNitro550 <79855501+MateoNitro550@users.noreply.github.com>
2021-08-08 18:01:54 -0500

    Update xxxwindow.sh

--- xxxwindow.sh
+++ xxxwindow.sh
@@ -4,16 +4,16 @@ WM_DESKTOP=$(xdotool getwindowfocus)
 
 if [ $WM_DESKTOP == "1883" ]; then
 
-	echo ""
+	echo "It is possible to add a name to the blank windows to something like 'empty', if we do not want the polybar to show us anything on the screen leave this blank"
 
 elif [ $WM_DESKTOP != "1883" ]; then
 
 	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
 	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | sed 's/"//' | rev | sed 's/"//' | rev)
 
-	if [ $WM_CLASS == 'Gnome-terminal' ]; then
+	if [ $WM_CLASS == 'Application name to change' ]; then
 
-		echo "Parrot Terminal"
+		echo "here goes the custom name"
 	else
 
 		echo $WM_NAME
